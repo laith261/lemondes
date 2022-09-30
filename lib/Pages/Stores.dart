0df8468f28f36537f9ec44259250841b5d8fb9b2
Laith Alrawe <laith261@gmail.com>
2022-09-30 14:21:26 +0300

    add Stores page

---
+++ lib/Pages/Stores.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+import 'package:lemondes/Widgets/for_all.dart';
+
+import '../Functions/Functions.dart';
+import '../Widgets/for_stores.dart';
+
+class Stores extends StatefulWidget {
+  const Stores({Key? key, required this.location}) : super(key: key);
+  final String location;
+
+  @override
+  State<Stores> createState() => _StoresState();
+}
+
+class _StoresState extends State<Stores> {
+
+
+  @override
+  Widget build(BuildContext context) {
+    double count =
+        (MediaQuery.of(context).size.width / 225).floorToDouble().toInt() + 1;
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text(widget.location.toUpperCase()),
+        ),
+        body:FutureBuilder<Map>(
+          future: https
+              .postMap({"location": widget.location}),
+          builder: (context,snap){
+            if(snap.hasData && snap.data!.isEmpty){
+              return const Empty();
+            }
+            if(snap.hasData && snap.data!.isNotEmpty){
+              return GridView.builder(
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: count.toInt()),
+                itemCount: snap.data!.length,
+                itemBuilder: (context, i) => Store(
+                  store: snap.data![i],
+                  location: widget.location,
+                ),
+              );
+            }
+            return const Loading();
+          },
+        )
+      ),
+    );
+  }
+}
---
+++ lib/Widgets/for_stores.dart
@@ -0,0 +1,70 @@
+// ignore_for_file: depend_on_referenced_packages
+
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:lemondes/Functions/Functions.dart';
+import 'package:lemondes/Functions/User.dart';
+import 'package:lemondes/Pages/Login.dart';
+import 'package:provider/provider.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:path/path.dart';
+
+import 'for_all.dart';
+
+class Store extends StatelessWidget {
+  const Store({
+    Key? key,
+    required this.store,
+    required this.location,
+  }) : super(key: key);
+  final Map store;
+  final String location;
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(5),
+      child: InkWell(
+        onTap: () {
+          if (context.read<User>().user == null && location != "Local") {
+            push(context, const Login());
+          } else {
+            // push(context,
+            //   location == "Local"
+            //       ? Products(
+            //     store: store['name'],
+            //   )
+            //       : Forms(
+            //     store: store['name'],
+            //   ),
+            // );
+          }
+        },
+        child: Column(
+          children: [
+            Expanded(
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(5),
+                child: extension(store["stimg"]) == ".svg"
+                    ? SvgPicture.network(
+                  "$link/uploads/${store["stimg"]}",
+                  placeholderBuilder: (context) => const Loading(),
+                  fit: BoxFit.cover,
+                  clipBehavior: Clip.none,
+                )
+                    : CachedNetworkImage(
+                  placeholder: (context, i) => const Loading(),
+                  imageUrl: "$link/uploads/${store["stimg"]}",
+                  fit: BoxFit.cover,
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 3,
+            ),
+            Text(store['name']),
+          ],
+        ),
+      ),
+    );
+  }
+}
